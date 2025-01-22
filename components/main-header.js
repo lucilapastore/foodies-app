76e124a2c19f7e5b2a73cf5be7e3b36878cdb90c
lucilapastore <lucilapastore@gmail.com>
2025-01-22 12:36:01 -0300

    refactor MainHeader component with navigation and styling

--- components/main-header.js
+++ components/main-header.js
@@ -1,10 +1,23 @@
 import logoImg from "@/assets/logo.png";
 import Link from "next/link";
+import classes from "./main-header.module.css";
 export default function MainHeader() {
-  return;
-  <header>
-    <Link href="/">
-      <img src={logoImg.src} alt="A plate with food on it" />
-    </Link>
-  </header>;
+  return (
+    <header className={classes.header}>
+      <Link className={classes.logo} href="/">
+        <img src={logoImg.src} alt="A plate with food on it" />
+        Next Level Meals
+      </Link>
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <Link href="/meals">Browse Meals</Link>
+          </li>
+          <li>
+            <Link href="/community">Foodies Community</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
