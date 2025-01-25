a856b6aa7cc81eb2745679d62cb86883305204e4
lucilapastore <lucilapastore@gmail.com>
2025-01-25 20:37:17 -0300

    add MealsGrid and MealItem components; enhance MealsPage layout and styles; initialize database with dummy meal data

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,25 @@
+import MealsGrid from "@/components/meals/mals-grid";
+import Link from "next/link";
+import classes from "./page.module.css";
+
 export default function MealsPage() {
   return (
-    <div>
-      <h1>Meals</h1>
-      <p>Here are all the meals</p>
-    </div>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
